325e7c0a82b84076fb30e83e8847f5627d4fc8b5
leonelguccione <leonel.guccione@gmail.com>
2020-05-05 07:49:33 -0300

    primer commit

---
+++ SerializacionBinaria/src/modelo/DVD.java
@@ -0,0 +1,38 @@
+package modelo;
+
+import java.io.Serializable;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class DVD implements Serializable
+{
+    private List movies = new ArrayList();
+
+    public DVD()
+    {
+    }
+
+    public List getMovies()
+    {
+        return movies;
+    }
+
+    public void setMovies(List movies)
+    {
+        this.movies = movies;
+    }
+
+    public String toString()
+    {
+        String stMovies = "";
+        Iterator iter = getMovies().iterator();
+        while(iter.hasNext())
+        {
+            Movie unaMovie = (Movie)iter.next();
+            stMovies = stMovies + unaMovie.toString();
+        }
+        return stMovies;
+    }
+}
---
+++ SerializacionXML/src/modelo/DVD.java
@@ -0,0 +1,36 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class DVD
+{
+    private List movies = new ArrayList();
+
+    public DVD()
+    {
+    }
+
+    public List getMovies()
+    {
+        return movies;
+    }
+
+    public void setMovies(List movies)
+    {
+        this.movies = movies;
+    }
+
+    public String toString()
+    {
+        String stMovies = "";
+        Iterator iter = getMovies().iterator();
+        while(iter.hasNext())
+        {
+            Movie unaMovie = (Movie)iter.next();
+            stMovies = stMovies + unaMovie.getName() + ", ";
+        }
+        return stMovies;
+    }
+}
